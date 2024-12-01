c7c98a5d19b636712a6a54ebf572444ed0495f9b
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2024-12-01 22:15:30 +0900

    :recycle: [Refactor] 요청사항 버튼, css 수정

--- src/page/user/costCall/comments/comment.tsx
+++ src/page/user/costCall/comments/comment.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import pageStyles from '../index.module.css';
+import style from './comment.module.css';
+import Button from '../../../../components/btn/Button';
 
 import 'react-calendar/dist/Calendar.css';
 import './customCalendar.css';
@@ -6,21 +9,45 @@ import './customCalendar.css';
 interface MovingCommentProps {
   value: string | null | undefined;
   onClick: (value: string) => void;
+  disabled: boolean;
 }
 
-export default function Comments({ value, onClick }: MovingCommentProps) {
+export default function Comments({
+  value,
+  onClick,
+  disabled,
+}: MovingCommentProps) {
+  const [comment, setComment] = useState<string>(value || '');
+
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    onClick(e.target.value);
+    setComment(e.target.value);
+  };
+
+  const handleSelectClick = (comment: string) => {
+    onClick(comment);
   };
 
   return (
     <div>
-      <div>
-        <div>요청 사항이 있으면 적어 주세요</div>
-        <textarea value={value || ''} onChange={handleChange} />
-      </div>
-      <div>
-        <button>견적 확정하기</button>
+      <div className={pageStyles.white}>요청 사항이 있으면 적어 주세요</div>
+      <div className={pageStyles.option}>
+        <div className={style.layout}>
+          <div className={style.text}>요청사항</div>
+          <textarea
+            className={style.textarea}
+            name='comment'
+            placeholder='요청사항을 입력해 주세요'
+            value={comment}
+            onChange={handleChange}
+          />
+          <Button
+            className={style.btn}
+            text='견적 확정하기'
+            style='solid640pxBlue300'
+            disabled={!disabled}
+            onClick={() => handleSelectClick(comment)}
+          />
+        </div>
       </div>
     </div>
   );
