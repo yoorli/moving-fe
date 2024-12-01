1a2213913ddaffe149c4e4d35547d72f408f3c23
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2024-12-01 22:14:48 +0900

    :recycle: [Refactor] 페이지 진행 바 수정

--- src/page/user/costCall/index.tsx
+++ src/page/user/costCall/index.tsx
@@ -63,7 +63,18 @@ export default function UserCostCallPage() {
           <div className={styles.navLayout}>
             <div className={styles.navText}>견적요청</div>
             <div className={styles.navBar}>
-              <div className={styles.navBarColor} />
+              <div
+                className={styles.navBarColor}
+                style={
+                  isSelectOption.arrival && isSelectOption.departure
+                    ? { width: '100%' }
+                    : isSelectOption.movingDate
+                      ? { width: '75%' }
+                      : isSelectOption.movingType
+                        ? { width: '50%' }
+                        : { width: '25%' }
+                }
+              />
             </div>
           </div>
         </div>
