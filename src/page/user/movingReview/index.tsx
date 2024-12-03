46bb04b9fecd3d8d74dc3d11fcebf1d7be25630b
mjpark <pmj9498@gmail.com>
2024-12-03 09:50:08 +0900

    :recycle: [Refactor] 모달 임시 배치 및 틀 잡기

--- src/page/user/movingReview/index.tsx
+++ src/page/user/movingReview/index.tsx
@@ -1,13 +1,22 @@
 import { useState } from 'react';
 import Tab from '../../../components/tab/Tab';
 import style from './index.module.css';
-import NoContents from '../../../components/nocontents/NoContents';
+// import NoContents from '../../../components/nocontents/NoContents';
+import ModalContainer from '../../../components/modal/ModalContainer';
+import WritingReview from './components/WritingReview';
 
 export default function UserMovingReview() {
   const [currentTab, setCurrentTab] = useState<'first' | 'second'>('first');
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleTabChange = (tab: 'first' | 'second') => {
     setCurrentTab(tab);
+    setIsModalOpen(true); // 임시로 모달 열리는 거 테스트하게끔 설정
+  };
+
+  const modalBtnClick = () => {
+    setIsModalOpen(false);
+    console.log('you clicked modal button :) ');
   };
 
   return (
@@ -19,11 +28,22 @@ export default function UserMovingReview() {
         selectedTab={currentTab}
         onTabChange={handleTabChange}
       />
-      <NoContents
+      {isModalOpen && (
+        <ModalContainer
+          title='지정 견적 요청하기'
+          isText={false}
+          buttonText='일반 견적 요청하기'
+          closeBtnClick={() => setIsModalOpen(!isModalOpen)}
+          buttonClick={modalBtnClick}
+        >
+          <WritingReview />
+        </ModalContainer>
+      )}
+      {/* <NoContents
         hasButton={true}
         buttonText='리뷰 작성하러 가기'
         buttonHandler={() => handleTabChange('first')}
-      />
+      /> */}
     </div>
   );
 }
