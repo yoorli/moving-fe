4e1e90cdd5ec45649d63aa8648b223fb5c4cb06e
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2024-12-06 17:30:29 +0900

    :bug: [Fix] holidayKr 오류 수정

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,16 +1,5 @@
 /// <reference types="react-scripts" />
 
-
-declare module 'holiday-kr' {
-  interface HolidayKR {
-    isHoliday(date: string): boolean;
-    getLunar(date: Date): Date;
-  }
-
-  const holidayKR: HolidayKR;
-  export = holidayKR;
-}
-
 declare global {
   interface Window {
     Kakao: any;
@@ -18,4 +7,3 @@ declare global {
 }
 
 export {};
-
