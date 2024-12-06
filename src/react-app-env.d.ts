37223db0d335318b94461bce3c81aaac99cb2004
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2024-12-06 15:10:43 +0900

    :recycle: [Refactor] 달력에 손없는 날 표시

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+declare module 'holiday-kr' {
+  interface HolidayKR {
+    isHoliday(date: string): boolean;
+    getLunar(date: Date): Date;
+  }
+
+  const holidayKR: HolidayKR;
+  export = holidayKR;
+}
