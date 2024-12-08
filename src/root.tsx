fd25cb7e4992264ad8c987e3a684c7851099c713
minseokim <claudia503@naver.com>
2024-12-08 13:47:30 +0900

    :sparkles: [Feat] root.tsx 기사님 상세 페이즈를 위한 /driver/:id 경로 추가 및 DriverDetailPage 연결

--- src/root.tsx
+++ src/root.tsx
@@ -21,6 +21,7 @@ import ServiceRandingPage from './page/root';
 import DriverRegisterPage from './page/driver/register';
 import DriverCostHandlerPage from './page/driver/costHandler';
 import SearchDriverForGuest from './page/root/searchDriver';
+import DriverDetailPage from './page/root/DriverDetailPage';
 
 const router = createBrowserRouter([
   {
@@ -30,6 +31,10 @@ const router = createBrowserRouter([
       {
         path: '/searchDriver',
         element: <SearchDriverForGuest />, // 비회원용 기사님 찾기 페이지
+      },
+      {
+        path: '/driver/:id',
+        element: <DriverDetailPage />, // 기사님 상세 페이지
       },
       {
         path: '/user/login',
