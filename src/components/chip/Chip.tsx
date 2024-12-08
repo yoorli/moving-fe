5f4f46726e9a27c816920936e00e8b74dd5bbf33
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2024-12-08 16:25:22 +0900

    :recycle: [Refactor] chip.module.css -> thip.module.css 로 임시 수젗

--- src/components/chip/Chip.tsx
+++ src/components/chip/Chip.tsx
@@ -1,5 +1,5 @@
 import { useMedia } from '../../lib/function/useMediaQuery';
-import style from './chip.module.css';
+import style from './Thip.module.css';
 import cn from 'classnames';
 import icHomeLarge from '../../assets/icons/ic_home_large.svg';
 import icHomeMedium from '../../assets/icons/ic_home_medium.svg';
