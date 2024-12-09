23c79c287fe40278912a087672cad1b67bfc3d93
mjpark <pmj9498@gmail.com>
2024-12-09 18:08:04 +0900

    :recycle: [Refactor] 대기 중인 견적 초기 설계

--- src/page/user/pendingCost/index.tsx
+++ src/page/user/pendingCost/index.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Tab from '../../../components/tab/Tab';
 import style from './index.module.css';
 import useDirection from '../../../lib/function/direction';
+import CostInfo from '../../../components/costInfo/CostInfo';
 
 export default function PendingCost() {
   const [currentTab, setCurrentTab] = useState<'first' | 'second'>('first');
@@ -24,7 +25,8 @@ export default function PendingCost() {
         secondTabRoute={direction_receivedCost}
       />
       <div className={style.container}>
-        <div>대기 중인 견적</div>
+        <CostInfo />
+        <div>카드 나열</div>
       </div>
     </>
   );
