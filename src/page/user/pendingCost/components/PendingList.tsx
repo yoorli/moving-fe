90fcdbb95dafd2e6ca463032468f76103abf6f5b
minseokim <claudia503@naver.com>
2024-12-11 16:32:18 +0900

    :sparkles: [Feat] 대기 중인 견적 및 받았던 견적페이지에서 기사님 카드 클릭 시 견적 상세페이지로 이동 추가

--- src/page/user/pendingCost/components/PendingList.tsx
+++ src/page/user/pendingCost/components/PendingList.tsx
@@ -1,15 +1,25 @@
+import { useNavigate } from 'react-router-dom';
 import DriverCard from '../../../../components/card/DriverCard';
 import { mockData } from './mockData';
 import style from './PendingList.module.css';
 
 export default function PendingList() {
-  console.log(mockData.list[0].career);
+  const navigate = useNavigate();
+
+  const handleCardClick = (id: number) => {
+    navigate(`/user/costDetail/${id}`); // 카드 클릭 시 견적 상세 페이지로 이동
+  };
+
   return (
     <div className={style.container}>
       <div className={style.cardContainer}>
         {mockData.list.map((cost, index) => (
-          // confirmCostBtn(견적 확정하기 버튼, detailBtn(상세보기 버튼) props
-          <DriverCard key={index} list={cost} type='waiting' />
+          <DriverCard
+            key={index}
+            list={cost}
+            type="waiting"
+            onClick={() => handleCardClick(cost.id)}
+          />
         ))}
       </div>
     </div>
--- src/page/user/receivedCostDetail/components/ReceivedList.tsx
+++ src/page/user/receivedCostDetail/components/ReceivedList.tsx
@@ -1,24 +1,33 @@
+import { useNavigate } from 'react-router-dom';
 import style from './ReceivedList.module.css';
 import DriverCard from '../../../../components/card/DriverCard';
 import { MoverList, Mover } from '../mockData';
 
 export default function ReceivedList({ list, count }: MoverList) {
+  const navigate = useNavigate();
+
+  const handleCardClick = (id: number) => {
+    navigate(`/user/costDetail/${id}`); // 카드 클릭 시 견적 상세 페이지로 이동
+  };
+
   return (
-    <>
-      <div className={style.infoContainer}>
-        <div className={style.infoTitle}>견적서 목록 ({count})</div>
-        <div className={style.infoMain}>
-          {list.length > 0 ? (
-            list?.map((mover: Mover) => (
-              <div key={mover.id} className={style.card}>
-                <DriverCard list={mover} type='cost' />
-              </div>
-            ))
-          ) : (
-            <div>받은 견적이 없습니다</div>
-          )}
-        </div>
+    <div className={style.infoContainer}>
+      <div className={style.infoTitle}>견적서 목록 ({count})</div>
+      <div className={style.infoMain}>
+        {list.length > 0 ? (
+          list.map((mover: Mover) => (
+            <div key={mover.id} className={style.card}>
+              <DriverCard
+                list={mover}
+                type="cost"
+                onClick={() => handleCardClick(mover.id)}
+              />
+            </div>
+          ))
+        ) : (
+          <div>받은 견적이 없습니다</div>
+        )}
       </div>
-    </>
+    </div>
   );
 }
