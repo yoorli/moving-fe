35e69b9b25a0aa04eed122f62f2b542773f94410
yoolri <lucca6042@gmail.com>
2024-12-12 13:07:07 +0900

    :sparkles: [Feat] 견적 상세 페이지 레이아웃

--- src/page/driver/costDetail/index.tsx
+++ src/page/driver/costDetail/index.tsx
@@ -1,10 +1,55 @@
-import style from './index.module.css'
+import UserCard from '../../../components/card/UserCard';
+
+import { useParams } from 'react-router-dom';
+import style from './index.module.css';
+
+import { mockData } from './mockData';
+import SnsShare from '../../../components/snsShare/SnsShare';
+import CostInfo from '../../../components/costInfo/CostInfo';
+import { useMedia } from '../../../lib/function/useMediaQuery';
+import { formatCurrency } from '../../../lib/function/utils';
 
 export default function DriverCostDetailPage() {
+  const isPc = useMedia().pc;
+  const id = useParams().id;
+  const user = mockData.users[Number(id) - 1];
+
+  const info = {
+    movingRequest: user.movingRequest,
+    movingType: user.movingType,
+    movingDate: user.movingDate,
+    departure: user.detailDeparture,
+    arrival: user.detailArrival,
+    comment: user.comment,
+  };
+
   return (
     <div className={style.container}>
       <nav className={style.navigation}>견적 상세</nav>
-      <div className={style.mainContainer}></div>
+      <div className={style.mainContainer}>
+        <div className={style.contents}>
+          <UserCard list={user} />
+          {!isPc && (
+            <div className={style.share}>
+              <SnsShare nickname={user.customerName} />
+            </div>
+          )}
+          <div className={style.estimate}>
+            <div className={style.label}>견적가</div>
+            {formatCurrency(user.price)}
+          </div>
+          <div className={style.estimateInfo}>
+            <div className={style.info}>
+              <CostInfo {...info} />
+            </div>
+          </div>
+        </div>
+        {isPc && (
+          <div className={style.share}>
+            <SnsShare nickname={user.customerName} />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
