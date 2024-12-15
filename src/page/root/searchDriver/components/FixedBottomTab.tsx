7c13c9df32a6cf8943ca329c6765c46136fa57c3
minseokim <claudia503@naver.com>
2024-12-15 10:07:19 +0900

    :sparkles: [Feat] 기사님 상세페이지 하단탭에 찜 상태 및 요청 상태에 따른 버튼 아이콘 변경, 활성화/비활성화 처리와 모달 열림 기능 추가

--- src/page/root/searchDriver/components/FixedBottomTab.tsx
+++ src/page/root/searchDriver/components/FixedBottomTab.tsx
@@ -2,22 +2,41 @@ import React from "react";
 import style from "./FixedBottomTab.module.css";
 import Button from "../../../../components/btn/Button";
 import HeartIcon from "../../../../assets/icons/ic_full_heart_small.svg";
+import HeartEmptyIcon from "../../../../assets/icons/ic_empty_heart_small.svg";
 
-const FixedBottomTab = () => {
+interface FixedBottomTabProps {
+  isFavorite: boolean;
+  isAssigned: boolean;
+  isConfirmed: boolean;
+  setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const FixedBottomTab = ({
+  isFavorite,
+  isAssigned,
+  isConfirmed,
+  setModalOpen,
+}: FixedBottomTabProps) => {
   return (
     <div className={style.fixedBottomTab}>
       <div className={style.container}>
         <Button
           btnStyle="outlined354pxLine200"
-          src={HeartIcon}
+          src={isFavorite ? HeartIcon : HeartEmptyIcon}
           alt="찜하기 아이콘"
           srcLocationFront
           className={style.heartButton}
         />
         <Button
-          text="지정 견적 요청하기"
+          text={isAssigned ? "지정 견적 요청 완료" : "지정 견적 요청하기"}
           btnStyle="solid354pxBlue300"
+          disabled={isAssigned}
           className={style.requestButton}
+          onClick={() => {
+            if (!isAssigned && !isConfirmed) {
+              setModalOpen(true);
+            }
+          }}
         />
       </div>
     </div>
@@ -25,3 +44,5 @@ const FixedBottomTab = () => {
 };
 
 export default FixedBottomTab;
+
+
