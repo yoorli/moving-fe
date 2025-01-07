112088380b50d56b96b52ee5da7b748f2baf099e
TradeOffEgoist <egoist116@naver.com>
2025-01-07 10:22:24 +0900

    :sparkles: [Feat] 일반유저 프로필 수정

--- src/page/user/editProfile/index.tsx
+++ src/page/user/editProfile/index.tsx
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { TextBtn } from '../../../components/page/edit/EditBtn';
 import style from './index.module.css';
 import { UserregisterFormValidation, UserregisterFormValues } from './type';
 import { ServiceRegion } from '../../../components/page/register/Region';
 import RegisterMidComponent from './components/Mid';
+import { translateServiceReverseRegion } from '../../../lib/function/utils';
+import { auth } from '../../../lib/api/auth';
+import { isAxiosError } from 'axios';
+import { AuthContext } from '../../../context/authContext';
 
 export default function UserEditProfilePage() {
+  const {
+    userValue: { user },
+  } = useContext(AuthContext);
+
   const [preview, setPreview] = useState<string | undefined>();
   const [values, setValues] = useState<UserregisterFormValues>({
     image: null,
@@ -67,6 +75,55 @@ export default function UserEditProfilePage() {
       }
     }
   };
+
+  const [isLoginPending, setIsLoginPending] = useState<boolean>(false);
+
+  const profileRegister = async () => {
+    if (
+      validation.region &&
+      validation.image &&
+      !!values.region &&
+      values.image &&
+      (!!values.small || !!values.house || !!values.office)
+    ) {
+      const keys = Object.keys(values) as [];
+
+      const formData = {
+        region: translateServiceReverseRegion(values.region as string),
+        profileImage: values.image,
+        serviceType: keys
+          .filter((data) => {
+            const service = ['office', 'small', 'house'];
+            return service.includes(data);
+          })
+          .filter((data) => {
+            return values[data] === 'on';
+          })
+          .map((data) => (data as string).toUpperCase()),
+      };
+
+      try {
+        setIsLoginPending(true);
+        await auth.patch('/customer/profile', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+        alert('프로필 수정이 완료 되었습니다.');
+        window.location.href = '/';
+      } catch (e) {
+        if (isAxiosError(e)) {
+          const data = e.response?.data;
+
+          alert(data);
+        }
+      } finally {
+        setIsLoginPending(false);
+      }
+    } else {
+      return;
+    }
+  };
   return (
     <div className={style.container}>
       <div className={style.wrapper}>
@@ -77,11 +134,12 @@ export default function UserEditProfilePage() {
           values={values}
           validation={validation}
           inputHeandler={inputHeandler}
-          preview={preview}
+          preview={preview ?? user?.Customer?.profileImage}
         />
         <div className={style.bottom}>
           <TextBtn
-            text='수정하기'
+            onClick={profileRegister}
+            text={isLoginPending ? 'loading...' : '수정하기'}
             validation={
               validation.region &&
               !!values.region &&
