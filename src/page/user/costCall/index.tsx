16e1900b6b1f8b9d698a7b639bd44feac3a3534f
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2025-01-10 14:04:00 +0900

    :recycle: [Refactor] PageError 추가, css 수정

--- src/page/user/costCall/index.tsx
+++ src/page/user/costCall/index.tsx
@@ -6,6 +6,9 @@ import CostCallContent from './components/CostCallContent';
 import useDirection from '../../../lib/function/direction';
 import { useGetCustomer } from '../../../lib/useQueries/customer';
 import LoadingSpinner from '../../../components/loading/LoadingSpinner';
+import PageError from '../../../components/pageError/PageError';
+
+import noItems from '../../../assets/icons/ic_noItems.svg';
 
 export interface SelectValues {
   movingType: boolean;
@@ -15,7 +18,7 @@ export interface SelectValues {
 }
 
 export default function UserCostCallPage() {
-  const { direction_pendingCost } = useDirection();
+  const { direction_pendingCost, direction_root } = useDirection();
   const [isSubmitted, setIsSubmitted] = useState(false);
 
   const [isSelectOption, setIsSelectOption] = useState<SelectValues>({
@@ -25,7 +28,25 @@ export default function UserCostCallPage() {
     arrival: false,
   });
 
-  const { data, isLoading } = useGetCustomer();
+  const { data, isLoading, error } = useGetCustomer();
+
+  const handleErrorClick = () => {
+    direction_root();
+  };
+
+  if (error) {
+    return (
+      <div className={style.noContent}>
+        <PageError
+          image={noItems}
+          contentTextFirst={'데이터를 불러오는 중 에러가 발생했습니다:'}
+          contentTextSecond={`${error.message}`}
+          buttonText='홈으로 돌아가기'
+          buttonHandler={handleErrorClick}
+        />
+      </div>
+    );
+  }
 
   return (
     <>
